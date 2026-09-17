63689bce7b0e5d6c8b0f03d971f831289dff9887
Crazyrobot321 <erikmz40@gmail.com>
2026-09-17 15:21:13 +0200

    GetLineValues_ShouldReturnCorrectNumber

---
+++ AoC2023D01.Tests/NumberFinderTest.cs
@@ -0,0 +1,22 @@
+﻿namespace AoC2023D01.Tests
+{
+    public class NumberFinderTest
+    {
+        [Theory]
+        [InlineData("1abc2", 12)]
+        [InlineData("pqr3stu8vwx", 38)]
+        [InlineData("a1b2c3d4e5f", 15)]
+        [InlineData("treb7uchet", 77)]
+        [InlineData("no digits here", 0)]
+        [InlineData(" ", 0)]
+        public void GetLineValues_ShouldReturnCorrectNumber(string input, int expected)
+        {
+            // Arrange
+            var numberFinder = new NumberFinder();
+            // Act
+            var result = numberFinder.GetLineValues(input);
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ AoC2023D01/NumberFinder.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode.Common;
+
+Console.WriteLine("Hello, World!");
+
+public class NumberFinder
+{
+    public int GetLineValues(string text)
+    {
+        var digits = text.Where(char.IsDigit).ToArray();
+        if (digits.Length == 0)
+        {
+            return 0;
+        }
+        return int.Parse($"{digits.First()}{digits.Last()}");
+    }
+}
